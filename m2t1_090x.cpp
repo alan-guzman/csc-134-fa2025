9c4796092aa8124985bff7d4525bfb4c5fe925b1
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-09-21 22:44:53 +0000

    set up 1st part of program

---
+++ m2t1_090x.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+#include <iostsream>
+using namespace std;
+
+
+int main() {
+
+    // Set up all variables
+    string first_name, last_name, full_name; // holds customer name
+    string product = "takis"; // change to whatever you like 
+    double cost_each = 0.99;
+    double total_cost;
+
+
+    // Greet the customer 
+    cout << "Welcome to our " << product << " store!" << endl;
+
+
+
+    // Ask how much they'd like to purchase
+
+    // Calculate total price 
+    // Give the result
