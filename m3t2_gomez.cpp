f48e7baa76878177cae7966fc7f24cd4edef33af
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-09-29 00:54:16 +0000

    started m3t2

---
+++ m3t2_gomez.cpp
@@ -0,0 +1,16 @@
+/*
+CSC-134-0901
+M3T2
+Alan Guzman-Gomez
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello" << endl;
+    cout << "world" << endl;
+    cout << endl;
+    return 0;
+}
