9785b2c2b319ccbefd6de6d9bf10579454ee8dce
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-10-01 14:38:40 +0000

    started M2LAB1

---
+++ M2LAB1.cpp
@@ -0,0 +1,28 @@
+/*
+CSC_134
+Alan Guzman-Gomez
+M2LAB1
+10/05/2024
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main ()
+{
+    // Constants for cost and amount charged
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+
+    // Variables 
+    double length,  // The crate's length
+        width,      // The crate's width
+        height,     // The crate's height 
+        volume,     // The volume of the crate
+        cost,       // The cost to build the crate 
+        charge,     // The customer charge for the crate
+        profit;     // The profit made on the crate 
+
+
+}
