87fd9bf67ed6ea155d281b6e433023536fce04da
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-10-04 16:59:36 +0000

    finished M3LAB1 !

--- M3LAB1.cpp
+++ M3LAB1.cpp
@@ -1,6 +1,6 @@
 /*
 CSC-134
-M3LAB1
+M3LAB1: Ex. 2
 Alan Guzman-Gomez
 10.04.25
 */
@@ -9,7 +9,29 @@ Alan Guzman-Gomez
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    
+    // Declare the variable we need
+    int choice;
 
-    return 0;
+    // Ask the question
+    cout << "Do you choose Cat 1 or Cat 2?" << endl;
+    cout << "Type 1 or 2: ";
+    cin >> choice;
+
+    // Using If, make decision based on the user's choice
+
+    if (1 == choice) {
+        cout << "Great choice! You chose Cat 1, a Siamese, a vocal and playful friend." << endl;
+    }
+    else if (2 == choice) {
+        cout << "Good choice. You chose Cat 2, a Tuxedo, a sassy and mean creature." << endl;
+    }
+    else {
+        cout << "I'm sorry, that is not a valid choice." << endl;
+    }
+
+    // Finish up
+    cout << "Thanks for playing!" << endl; // this runs no matter the choicr
+
+    return 0; // no errors
 }
