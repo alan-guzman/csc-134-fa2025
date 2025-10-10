07b6e48909cce64bacd3f1997915575b9d89dc49
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-10-10 22:46:42 +0000

    establishing variables & letter grades

---
+++ M3LAB2.cpp
@@ -0,0 +1,44 @@
+/* 
+CSC-134
+M3LAB2
+Alan Guzman-Gomez
+10.10.25
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    // Declare variables 
+    int grade;
+    char letterGrade;
+
+    // Input
+
+
+    // Establish letter grade 
+    if (grade >= 90 && grade <= 100) {
+        letterGrade = "A";
+    }
+    else if (grade >= 80 && grade <= 89) {
+        letterGrade = "B";
+    }
+    else if (grade >= 70 && grade <= 79) {
+        letterGrade = "C";
+    }
+    else if (grade >= 60 && grade <= 69) {
+        letterGrade = "D";
+    }
+    else {
+        letterGrade = "F";
+    }
+
+    // Ask user for input 
+     
+
+    // Display result
+
+
+
+}
