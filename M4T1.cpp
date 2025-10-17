4d1ece2de63ef66ff73a1f6d2d72315c85163375
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-10-17 19:55:51 +0000

    second program works, M4T1 finished

--- M4T1.cpp
+++ M4T1.cpp
@@ -21,5 +21,25 @@ int main() {
     }
     cout << "That's all!\n";
 
+    cout << "---------------------------" << endl;
+
+    cout << "        " << endl;
+
+    cout << "Program 5-6" << endl;
+    cout << "-----------" << endl;
+
+    const int MIN_NUMBER = 1,  // Starting number to square
+             MAX_NUMBER = 10; // Maximum number to square
+
+    int num = MIN_NUMBER;     // Counter
+
+    cout << "Number Number Squared\n";
+    cout << "---------------------\n";
+        while (num <= MAX_NUMBER)
+        {
+            cout << num << "\t\t" << (num * num) << endl;
+            num++; // Increment the counter
+        }
+
     return 0; // done
 }
