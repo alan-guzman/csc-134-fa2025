f2720fd7d8eafff5bc08ec85e3894324e26f9d84
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-10-17 14:13:48 +0000

    program is running, first question works !

---
+++ M3HW1.cpp
@@ -0,0 +1,30 @@
+/*
+CSC-134
+M3HW1 - Gold
+Alan Guzman-Gomez
+10/17/2025
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Question 1: ChatBot Program
+cout << "Question 1" << endl;
+
+cout << "Hello, I'm a C++ program! Do you like me? PLease type yes or no." << endl;
+
+string answer;
+cin >> answer;
+
+if (answer == "Yes") {
+    cout << "That's great! I'm sure we'll get along." << endl;
+} else if (answer == "No") 
+{
+    cout << "Well, maybe you'll learn to like me later." << endl;
+} else {
+    cout << "If you're not sure... that's OK." << endl;
+}
+
+    return 0;
+}
