805f8d8534b65a698f3d370b743dffcdbd8cf447
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-10-17 19:40:36 +0000

    starting code

---
+++ M4T1.cpp
@@ -0,0 +1,20 @@
+/*
+CSC-134
+M4T1
+Alan Guzman-Gomez
+10.17.2025
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Part 1, just say "hi" five times 
+    int count = 1;
+    while (count < 5) {
+        cout << "Hello number " << count << endl;
+    }
+
+    return 0; // done
+}
