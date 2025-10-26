a8895c185b167c685cb6cf10e3ff652be12494e1
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-10-26 02:12:55 +0000

    finished m4lab1

--- M4LAB1.cpp
+++ M4LAB1.cpp
@@ -14,13 +14,36 @@ int main() {
     int height, width;
 
     // Start with a set size
-    height = 5;
-    width = 5;
+    // To finish, use variable sizes, by asking the user
+    // height = 5;
+    // width = 5;
+    cout << "How tall should the block be?" << endl;
+    cin >> height;
+    cout << "How wide should the block be?" << endl;
+    cin >> width;
 
     // Test: make a row of asterisks
+    cout << "One row" << endl;
     for (int i=0; i < width; i++) {
         cout << "*" << " ";
     }
     cout << endl; // finish the row
+
+    // Test: make a column
+    cout << "One column" << endl;
+    for (int j=0; j < height; j++) {
+        cout << "*" << endl;
+    }
+
+    // Instead of printing a column, we want to print a row within the outer loop
+    // (nested loops)
+    cout << "The entire block" << endl;
+    for (int j=0; j < height; j++) {
+        // Print one row
+        for (int i=0; i < width; i++) {
+            cout << "*" << " ";
+        }
+        cout << endl; // End the row with a newline
+    }
 
 }
