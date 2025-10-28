74df9c47ba7c5c82e5876001d86d7407cbbeba25
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-10-28 14:51:54 +0000

    fixed bugs, finished assignment

--- M4HW1.cpp
+++ M4HW1.cpp
@@ -14,13 +14,17 @@ int main() {
 int userNum;
 int multiplier = 1;
 
+// Loop
 do {
     cout << "Enter a number from 1 to 12: " << endl;
 
     if (!(cin >> userNum)) {
     cout << "Invalid input. Number must be between 1 and 12. Please retry." << endl;
+    cin.clear();
+    cin.ignore(100, '\n');
+    userNum = 0;
     }
-    if (userNum < 1 || userNum > 12) {
+    else if (userNum < 1 || userNum > 12) {
     cout << "Invalid input. Number must be between 1 and 12. Please retry." << endl;
     }
 } while (userNum < 1 || userNum > 12);
