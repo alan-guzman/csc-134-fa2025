8b8e60393d1bfd47cdc179677397e73d98888ac3
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-10-28 14:33:02 +0000

    finished gold !! feeling better

--- M4HW1.cpp
+++ M4HW1.cpp
@@ -14,11 +14,16 @@ int main() {
 int userNum;
 int multiplier = 1;
 
-cout << "Enter a number from 1 to 12: " << endl;
+do {
+    cout << "Enter a number from 1 to 12: " << endl;
 
-if (!(cin >> userNum)) {
-    cout << "Invalid input." << endl;
-}
+    if (!(cin >> userNum)) {
+    cout << "Invalid input. Number must be between 1 and 12. Please retry." << endl;
+    }
+    if (userNum < 1 || userNum > 12) {
+    cout << "Invalid input. Number must be between 1 and 12. Please retry." << endl;
+    }
+} while (userNum < 1 || userNum > 12);
 
 while (multiplier<= 12) {
     int answer = userNum * multiplier;
