60b1e4119948b697764f7450905ce40fc8b04d65
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-11-07 13:11:32 +0000

    finished part 1

---
+++ M5T2.cpp
@@ -0,0 +1,25 @@
+/*
+CSC-134
+M5T2
+Alan Guzman-Gomez
+11.06.25
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int count = 0;
+    int result;
+
+    while (count < 30) {
+        // What is 100 times the count
+        result = 100 * count;
+        cout << count << " " << result << endl;
+        count++;
+    }
+
+
+
+}
