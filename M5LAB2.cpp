3da1dd002263a044a413e4ab08ac314f2394286b
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-11-10 04:51:05 +0000

    started m5lab2

---
+++ M5LAB2.cpp
@@ -0,0 +1,43 @@
+/*
+CSC-134
+M5LAB2 
+Alan Guzman-Gomez
+11.09.25
+*/
+
+#include <iostream>
+using namespace std;
+
+// Declare (Write the prototypes for)
+// the getLength,
+// getWidth, getArea, and displayData
+// functions here.
+
+int main()
+{
+	// This program calculates the area of a rectangle.
+	// TODO: fix any syntax errors
+	
+   double length,    // The rectangle's length
+          width,     // The rectangle's width
+          area;      // The rectangle's area
+          
+   // Get the rectangle's length.
+   length = getLength();
+   
+   // Get the rectangle's width.
+   width = getWidth()
+   
+   // Get the rectangle's area.
+   are = getArea();
+   
+   // Display the rectangle's data.
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//***************************************************
