2eb39a67429bef30b4ceaaf064b860520dc6cb76
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-11-11 07:18:27 +0000

    other two files did not work, trying new one

--- M5LAB2.cpp
+++ M5LAB2.cpp
@@ -6,36 +6,74 @@ Alan Guzman-Gomez
 */
 
 #include <iostream>
+#include <iomanip>
+#include <limits>
 using namespace std;
 
-// Declare (Write the prototypes for)
-// the getLength,
-// getWidth, getArea, and displayData
-// functions here.
+double getLength();
+double getWidth();
+double getArea(double length, double width);
+void displayData(double length, double width, double area);
+int get_choice(int size);
 
 int main()
 {
 	// This program calculates the area of a rectangle.
 	// TODO: fix any syntax errors
-	
-   double length,    // The rectangle's length
-          width,     // The rectangle's width
-          area;      // The rectangle's area
-          
-   // Get the rectangle's length.
-   length = getLength();
-   
-   // Get the rectangle's width.
-   width = getWidth()
-   
-   // Get the rectangle's area.
-   are = getArea();
-   
-   // Display the rectangle's data.
-   displayData(length, width, area);
-          
-   return 0;
+
+      double length = getLength();
+      double width = getWidth();
+      double area = getArea(length, width);
+
+      displayData(length, width, area);
+
+      return 0;
 }
+
+      double getLength() {   
+            double length;
+            cout << "Enter rectangle length: " << endl;
+            cin >> length;
+            return length;
+      }
+
+      double getwidth() {
+            double width;
+            cout << "Enter rectangle width: " << endl;
+            cin >> width;
+            return width;
+      }    
+
+      double getArea(double length, double width) {
+            return length * width;
+      }
+
+      void displayData(double length, double width, double area) {
+            cout << fixed << setprecision(2); 
+            cout << "Length: " << length << endl;
+            cout << "Width: " << width << endl;
+            cout << "Area: " << area << endl;
+
+      }
+
+          
+int get_choice(int size) {
+  // Only allow choices between 1 and size
+  int choice=0;
+  while (choice < 1 || choice > size) {
+    cout << "Choose: ";
+    cin >> choice;
+        // Check if the input is valid
+        if (cin.fail()) {
+          cin.clear(); // Clear the error flag
+          cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+          cout << "Invalid input. Please enter a number between 1 and " << size << "." << endl;
+        }
+  }
+  cout << "You picked option #" << choice << endl;
+  return choice;
+}
+          
 
 //***************************************************
 // TODO: write the getLength, getWidth, getArea,    *
