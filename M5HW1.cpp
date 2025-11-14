3b2d7e9a15bedacbba92fb38acea75959739a2ac
Alan Guzman-Gomez <arlene.g398@gmail.com>
2025-11-14 01:06:08 +0000

    question 3 done, halfway done with homework !!

--- M5HW1.cpp
+++ M5HW1.cpp
@@ -70,6 +70,56 @@ int main () {
     cout << "Hyperrectangle Volume: " << fixed << setprecision(4) <<
     volume << " square units" << endl;
 
+    // Question 3
+    cout << "------------------------------------------" << endl;
+    cout << "              Question Three              " << endl;
+    cout << "------------------------------------------" << endl; 
+
+    int number;
+    string roman_numeral;
+
+    cout << "Enter a number (1-10): ";
+    cin >> number;
+
+    switch (number) {
+        case 1: 
+            roman_numeral = "I";
+            break;
+        case 2:
+            roman_numeral = "II";
+            break;
+        case 3:
+            roman_numeral = "III";
+            break;
+        case 4:
+            roman_numeral = "IV";
+            break;
+        case 5:
+            roman_numeral = "V";
+            break;
+        case 6: 
+            roman_numeral = "VI";
+            break;
+        case 7:
+            roman_numeral = "VII";
+            break;
+        case 8:
+            roman_numeral = "VIII";
+            break;
+        case 9:
+            roman_numeral = "IX";
+            break;
+        case 10:
+            roman_numeral = "X";
+            break;
+        default:
+            roman_numeral = "Invalid, try again.";
+            break;
+    }
+
+    cout << "The Roman numeral version of " << number << " is " 
+    << roman_numeral << "." << endl;
+
     return 0;
 }
 
